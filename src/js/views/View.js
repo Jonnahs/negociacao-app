2ff659a5f63125e2bb1a82a80602199f44eede5e
Jonnahs <jonas.desenvolvimento@gmail.com>
2021-04-09 22:15:42 -0300

    Ajuste js no gitignore

--- src/js/views/View.js
+++
@@ -1,8 +0,0 @@
-class View {
-    constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
-    }
-    update(model) {
-        this._elemento.innerHTML = this.template(model);
-    }
-}
